6cdca7ac504ba519a04cff461847d4b18f50420f
Karen <neydekaren@dcc.ufba.br>
2018-12-13 18:15:10 -0300

    Add files via upload

---
+++ mainwindow.h
@@ -0,0 +1,40 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMovie>
+#include <QVideoWidget>
+#include <QMediaPlayer>
+#include <QMediaPlaylist>
+#include <QFileDialog>
+#include <QMessageBox>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    QString moviePathPast;
+
+public slots:
+    void openMovie();
+    void changeSliderNumber ();
+
+signals:
+    void pathChanged();
+
+private:
+    Ui::MainWindow *ui;
+    QMovie *moviePlayer;
+    QMediaPlayer *mediaPlayer;
+    QMediaPlaylist *playList;
+    QMessageBox *msg;
+};
+
+#endif // MAINWINDOW_H
